8b9234270f898ed0e8346cdb2d4b7e2c6f2fbb32
gpol93 <36421639+gpol93@users.noreply.github.com>
2018-05-12 09:48:36 +0000

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -6,6 +6,8 @@ dataFile <- "./data/household_power_consumption.txt"
 #Transforming variables
 time <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S")  
  glob_active_power <- as.numeric(subSetData$Global_active_power) 
+
+#Plotting and saving variables
  png("plot2.png", width=480, height=480) 
  plot(time,  glob_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)") 
  dev.off() 
